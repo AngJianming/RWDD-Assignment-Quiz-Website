3eecc5d6c05490e0b8726aaa62a9098a047ee8af
AngJianming <angjianming2005@hotmail.com>
2024-12-02 20:11:10 +0800

    updates

--- Student/Student-Dashboard/Student-Dashboard.php
+++ Student/Student-Dashboard/Student-Dashboard.php
@@ -1,11 +1,3 @@
-<?php
-session_start();
-include("Constants/Header.php");
-include("Constants/Footer.php");
-include("Constants/Sidebar-Student.php");
-// include("connection.php");
-?>
-
 <!DOCTYPE html>
 <html lang="en" dir="US">
 
@@ -17,86 +9,94 @@ include("Constants/Sidebar-Student.php");
 
     <!-- Fluent UI CDN Link -->
     <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/fabric.min.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css">
+    <div class="sidebar">
+        <?php include("/xampp/htdocs/RWDD-Assignment-Quiz-Website/Constants/Sidebar-Student.php"); ?>
+    </div>
 </head>
 
 <body class="ms-Fabric">
 
-    <!-- Main Page content will be here, don't mix with side-bar stuff -->
-    <div class="home_content">
 
-        <!-- Content home principal page -->
-        <div class="wrapper">
+    <!-- Header -->
 
-            <!-- Header -->
-            <header class="header">
-                <?php include ("/Constants/Footer.php"); ?>
-            </header>
+    <?php include("/xampp/htdocs/RWDD-Assignment-Quiz-Website/Constants/Header.php"); ?>
 
-            <!-- Main content -->
-            <div class="content image-section">
-                <p>Image</p>
-            </div>
+    <main>
+        <article>
+            <section class="section hero" id="home" aria-label="home" style="background-image:url(hero-bg.jpg);">
+                <div class="container">
+                    <div class="hero-content">
+                        <p class="hero-subtitle" data-aos="zoom-in-right" data-aos-duration="1000" data-aos-delay="900">
+                            Code Combat</p>
+                        <h1 class="hero-title h1" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="1100">
+                            Quiz <span class="span">Levels</span> Matches</h1>
+                        <p class="hero-text" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="1300">
+                            Solve Python related question and compete with others in the global leaderboard.
+                        </p>
+                        <button class="btn skewBg" data-aos="zoom-out" data-aos-duration="1000"
+                            data-aos-delay="1500">Play</button>
+                    </div>
+                    <figure class="hero-banner img-holder">
+                        <img src="/img/RWDD bg-remover logo.png" alt="logo banner" class="w-100" data-aos="zoom-in-left"
+                            data-aos-duration="2500">
+                    </figure>
+                </div>
+            </section>
+        </article>
+    </main>
+    <script src="script.js" defer></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init({ offset: 0 });
+    </script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
-            <div class="content play-section">
-                <p>Contents</p>
-                <a href="">
-                    <button class="play-button">Play</button>
-                </a>
-            </div>
+    <!-- VV Footer VV -->
 
-            <!-- VV Footer VV -->
-            <footer>
-                <?php include ("/Constants/Footer.php"); ?>
-            </footer>
+    <?php include("/xampp/htdocs/RWDD-Assignment-Quiz-Website/Constants/Footer.php"); ?>
 
-        </div>
 
-        <div>
-            <div class="wave"></div>
-            <div class="wave"></div>
-            <div class="wave"></div>
-        </div>
-
-    </div>
-    <script src="/Student/Student-Dashboard/Student-Dashboard.js"></script>
 </body>
 
 </html>
+
 <!-- 
 <!DOCTYPE html>
 <html>
 <head>
 <style>
 div {
-  border: 1px solid gray;
-  padding: 8px;
+border: 1px solid gray;
+padding: 8px;
 }
 
 h1 {
-  text-align: center;
-  text-transform: uppercase;
-  color: #4CAF50;
+text-align: center;
+text-transform: uppercase;
+color: #4CAF50;
 }
 
 p {
-  text-indent: 50px;
-  text-align: justify;
-  letter-spacing: 3px;
+text-indent: 50px;
+text-align: justify;
+letter-spacing: 3px;
 }
 
 a {
-  text-decoration: none;
-  color: #008CBA;
+text-decoration: none;
+color: #008CBA;
 }
 </style>
 </head>
 <body>
 
 <div>
-  <h1>text formatting</h1>
-  <p>This text is styled with some of the text formatting properties. The heading uses the text-align, text-transform, and color properties.
-  The paragraph is indented, aligned, and the space between characters is specified. The underline is removed from this colored
-  <a target="_blank" href="tryit.asp?filename=trycss_text">"Try it Yourself"</a> link.</p>
+<h1>text formatting</h1>
+<p>This text is styled with some of the text formatting properties. The heading uses the text-align, text-transform, and color properties.
+The paragraph is indented, aligned, and the space between characters is specified. The underline is removed from this colored
+<a target="_blank" href="tryit.asp?filename=trycss_text">"Try it Yourself"</a> link.</p>
 </div>
 
 </body>
